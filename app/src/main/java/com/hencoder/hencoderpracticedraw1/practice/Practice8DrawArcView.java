476d911e0ae062b4d8341ba9bd476b4c343a2f09
tijun <qqgeshe@163.com>
2017-07-22 00:19:28 +0800

    practice 8

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,7 +2,10 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,10 +23,19 @@ public class Practice8DrawArcView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(300, 100, 700, 300, 180, 50, false, paint);
+
+        canvas.drawArc(300, 100, 700, 300, 20, 140, false, new Paint());
+
+        canvas.drawArc(300, 100, 700, 300, 240, 120, true, new Paint());
+
     }
 }
